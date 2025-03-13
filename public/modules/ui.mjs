61689d5154b3661b92861ed912aba5e600efeda5
hogstol03 <juliabhogstol@hotmail.com>
2025-03-14 00:53:03 +0100

    La til mer notifikasjon, samt fikset ferdig nedtelling notifiksjonen

--- public/modules/ui.mjs
+++ public/modules/ui.mjs
@@ -1,16 +1,25 @@
+// ui.mjs
+
 export function changeBackground() {
     const startView = document.getElementById('start-view');
     const timerView = document.getElementById('timer-view');
 
     if (startView.style.display === 'block') {
-
         document.body.style.backgroundImage = "url('./icons/eggcellentBG.png')";
-        //console.log("Start view active. Background set to eggcellentBG.");
-
     } else if (timerView.style.display === 'block') {
-        
         document.body.style.backgroundImage = "url('./icons/eggcellentTimerBG.png')";
-        //console.log("Timer view active. Background set to eggcellentTimerBG.");
+    }
+}
+
+export function requestNotificationPermission() {
+    if ("Notification" in window && Notification.permission !== "granted") {
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                console.log("Notification permission granted.");
+            } else {
+                console.log("Notification permission denied.");
+            }
+        });
     }
 }
 
