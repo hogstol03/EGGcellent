bb5786532acbfcef94e72d8b6bce8311dc528f8d
hogstol03 <juliabhogstol@hotmail.com>
2025-03-14 09:16:52 +0100

    test

--- public/app.js
+++
@@ -1,9 +0,0 @@
-import { startCountdown } from './modules/timer.js';
-
-document.getElementById("fiveMinButton").addEventListener("click", () => startCountdown(5, handleTimerFinish));
-document.getElementById("eightMinButton").addEventListener("click", () => startCountdown(8, handleTimerFinish));
-document.getElementById("tenMinButton").addEventListener("click", () => startCountdown(10, handleTimerFinish));
-
-function handleTimerFinish(minutes) {
-    alert(`${minutes} minutter timeren er ferdig!`);
-}
