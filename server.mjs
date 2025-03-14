ef0f0dccf12fbc9e9e1fa957c813507ac56b24ad
hogstol03 <juliabhogstol@hotmail.com>
2025-03-14 09:15:26 +0100

    La til logger-middleware for å logge innkommende requests

--- server.mjs
+++ server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import apiRoutes from './server/routes/api.mjs';
+import logger from './server/middleware/logger.mjs';
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use(logger);
 
 app.use('/api', apiRoutes);
 
